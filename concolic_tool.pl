f5805ff35605634defe9dcab3f331a2a4c395dd3
Sophie Fortz <sophie.fortz@student.unamur.be>
2019-03-21 18:55:57 +0100

    Some cleaning for the Z3 parts

--- concolic_tool.pl
+++ concolic_tool.pl
@@ -13,7 +13,7 @@
 :- dynamic depthk/1. %% max term depth
 :- dynamic interactive/0. %% used to decide which clause of write_form to use
 
-main :- catch(call_with_time_limit(10, mainT), X, error_process(X)).
+main :- catch(call_with_time_limit(1000000, mainT), X, error_process(X)).
 
   error_process(time_limit_exceeded) :- write('Timeout exceeded'), halt.
   error_process(X) :- write('Unknown Error' : X), nl, halt.
@@ -284,7 +284,7 @@ cleaning :-
 change_label(Label,[_],[Label]) :- !.
 change_label(Label,[X|R],[X|RR]) :- change_label(Label,R,RR).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % main transition rules
 
 % success
@@ -316,12 +316,12 @@ eval([A|_RA],[B|_RB],_GroundTerms,Trace,TraceR,Alts,NewAlts) :-
   reverse([(Bcopy2NoLabel,ListAllLabels)|Alts],NewAlts).
 
 % END main transition rules
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % some pretty-printing utilities..
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 print_testcases([]).
 print_testcases([testcase(A,_)|R]) :-
@@ -333,9 +333,9 @@ print_atom(X) :- copy_term(X,C),numbervars(C,0,_),print(C).
 println_atom(X) :- copy_term(X,C),numbervars(C,0,_),print(C),nl.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Fred's stuff
+% Sophie's stuff
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-/*
+
 matches_aux(A,HNegs,HPos,VarsToBeGrounded) :-
         writeln(in-matches_aux(A,HNegs,HPos,VarsToBeGrounded)),
 	%
@@ -345,95 +345,21 @@ matches_aux(A,HNegs,HPos,VarsToBeGrounded) :-
 	compound_name_arguments(A,_,[Var]),
 	get_constraints(Var,HNegs,HPos,Consts),
 	solve(Consts,Mod,Sat),
-	%%%%,
+	%
 	(Sat -> 
-	    split_model(Mod,_,ValsMod),
+	    split_model(Mod,ValsMod),
 	    z3_to_term_list(ValsMod,Terms),
-            append(VarsToBeGrounded, _, Terms), %% Verif que c'est OK si N > 2 ...	
-	    writeln(out-matches_aux(A,HNegs,HPos,VarsToBeGrounded)),nl
+            append(VarsToBeGrounded, _, Terms) %% Verif que c'est OK si N > 2	
             ;
-         nl,false
-        ).*/
+         nl,Sat
+        ),	
+        writeln(out-matches_aux(A,HNegs,HPos,VarsToBeGrounded)),
+        nl,Sat.
+
         
-z3_to_term_list([],[]).
-z3_to_term_list([T|Z3Terms],Terms) :-
-        z3_to_term(T,Term),
-        z3_to_term_list(Z3Terms,Terms_),
-        append([Term],Terms_,Terms).
-
-z3_to_term(Z3Str,Term) :-
-        sub_string(Z3Str, 0, 1, _, "("),
-        sub_string(Z3Str, _, 1, 0, ")"),
-        sub_string(Z3Str, 1, _, 1, Str),!, 
-        split_string(Str, " ", "", [Name|Args]),
-        get_str_args(Args,StrArgs),
-        string_concat(Name,"(",Str1),
-        string_concat(Str1, StrArgs, Str2),
-        string_concat(Str2, ")", Str3),
-        term_string(Term, Str3).
-z3_to_term(Z3Str,Term) :- term_string(Term, Z3Str),!. 
-
-get_str_args([A],A).
-get_str_args([A|Args],StrArgs) :- 
-        get_str_args(Args,StrArgs_),
-        string_concat(A,",",Str),
-        string_concat(Str, StrArgs_, StrArgs).
-    
-/* Splits a model (in String form) into two lists: one containing the "varriable-value" pairs, and the other with only the values.*/ 
-split_model(Model,List,Vals) :- 
-    split_string(Model, "\n", "\s\t\n", L),
-    split_affectation(L, List, Vals).    
-
-/* Splits a list of affectations (from a model) into two lists: one containing the "varriable-value" pairs, and the other with only the values.*/
-split_affectation([],[],[]).      
-split_affectation([H],L,[Affect]) :-
-    split_string(H, "->", " ", [Var,"",Affect]),
-    L = [(Var,Affect)].         
-split_affectation([H|T],L,Vals) :-
-    split_affectation(T,L1,Vals1),
-    split_string(H, "->", " ", [Var,"",Affect]),
-    append([(Var,Affect)],L1,L),
-    append([Affect],Vals1,Vals).      
-  
-
-matches_aux(A,HNegs,HPos,VarsToBeGrounded) :-
-	writeln(in-matches_aux(A,HNegs,HPos,VarsToBeGrounded)),
-	%
-	% check the preconditions:
-	preconds(A,HNegs,HPos,VarsToBeGrounded),
-	%
-	positive_atoms([A|HPos],B,UVariablesInB),
-	%
-	copy_term(A-VarsToBeGrounded,Ac-VarsToBeGroundedc),
-	unifiable(Ac,B,Theta),
-	% NB: now we use the new copy. The newest variables comes as lhs in theta
-	% hence u-Variables (which are older) are on rhs, so that grounding_subst
-	% has a chance to leave the u-variables unbounded.
-	%
-	grounding_subst(VarsToBeGroundedc,Theta,B,Eta),
-	%
-	compose_subst(Theta,Eta,Sigma),
-	%
-	domain(Sigma,B,DomSigmaB),
-	strict_empty_intersection(DomSigmaB,UVariablesInB),
-	%
-	apply_term_subst_term(Ac,Sigma,ASigma),
-	forall(member(Hneg,HNegs),\+ (ASigma=Hneg)),
-	%
-	% sanity test: it should always succeed by property of the PosNeg
-	(   forall(member(Hp,HPos),\+ \+ (ASigma=Hp))
-	->  true
-	;   throw(bug-matches_aux(Ac,HNegs,HPos,VarsToBeGrounded))),
-	%
-	A=ASigma,
-	writeln(out-matches_aux(A,HNegs,HPos,VarsToBeGrounded)),
-	true.
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% La suite n'est utilisée QUE dans matches_aux2
+% Checking the preconditions for solving constraints
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 preconds(A,HNegs,HPos,VarsToBeGrounded) :-
 	forall(member(Hp,HPos),Hp=A),
@@ -441,7 +367,66 @@ preconds(A,HNegs,HPos,VarsToBeGrounded) :-
 	term_variables(A,VarsA), term_variables(HPos-HNegs,VarsHs),
 	strict_empty_intersection(VarsA,VarsHs),
 	strict_is_largely_incuded(VarsToBeGrounded,VarsA).
+  
+strict_mem(X,[Y|_]) :- X == Y, !.
+strict_mem(X,[_|Ys]) :- strict_mem(X,Ys).
+strict_not_mem(X,Xs) :-
+	strict_not_mem_(Xs,X).
 
+  strict_not_mem_([],_X).
+  strict_not_mem_([Y|Zs],X) :-
+	Y \== X,
+	strict_not_mem_(Zs,X).
+
+strict_empty_intersection([],_).
+strict_empty_intersection([X|Xs],Vars) :-
+	strict_not_mem(X,Vars),
+	strict_empty_intersection(Xs,Vars).
+	
+strict_is_largely_incuded([],_).
+strict_is_largely_incuded([X|Xs],Ys) :-
+	strict_mem(X,Ys),
+	strict_is_largely_incuded(Xs,Ys).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Writing the constraints in a correct form for the Z3 interface
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+get_constraints(A,HNegs,HPos,Constrs) :- 
+        get_pos_consts(A,HPos,PosConsts),
+        get_neg_consts(A,HNegs,NegConsts),
+        append(PosConsts,NegConsts,Constrs).	
+
+%%  GERER PLUSIEURS ARGUMENTS !!! %%
+get_pos_consts(_,[],[]).
+get_pos_consts(A,[H|T],Constrs) :-
+        compound_name_arguments(H,_,[Args]),
+        C1 = [[A = (Args)]],
+        get_pos_consts(A,T,C2),
+        append(C1,C2,Constrs).
+
+%%  GERER PLUSIEURS ARGUMENTS !!! %%
+get_neg_consts(_,[],[]).
+get_neg_consts(A,[H|T],Constrs) :-
+        compound_name_arguments(H,_,[Args]),
+	copy_term(Args,CArgs),
+	term_variables(CArgs,VCArgs),
+        forall_terms(VCArgs,A,CArgs,C1),
+        get_neg_consts(A,T,C2),
+        append([[C1]],C2,Constrs).
+
+forall_terms([],A,Args,Pred):- Pred = (A \= (Args)).   
+forall_terms([H|T],A,Args,Pred) :-
+        forall_terms(T,A,Args,Pred1),
+        Pred = forall(var(H),Pred1). 
+        
+        
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Z3 solver
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Modif pour > 3 contraintes
 solve([C1,C2,C3],Model,Sat) :- 
     copy_term((C1,C2,C3),(CC1,CC2,CC3)),
     z3_mk_config,
@@ -473,281 +458,58 @@ solve([C1,C2,C3],Model,Sat) :-
         get_model_varT_eval(N,VVS,Values),
         term_variables([CC1,CC2,CC3],AllVars),
         AllVars=Values,
-        %format("Solved formulas: "),println([CC1,CC2,CC3]),
         Sat = true
     ;
         Sat = false
     ),
     z3_pop(N),
     z3_del_solver(N),
-    z3_del_context(N).
+    z3_del_context(N).	
+        
+        
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Spliting a Z3 (string) model into Prolog terms
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     
+split_model(Model,Vals) :- 
+    split_string(Model, "\n", "\s\t\n", L),
+    split_affectation(L, Vals).    
 
-get_constraints(A,HNegs,HPos,Constrs) :- 
-        get_pos_consts(A,HPos,PosConsts),
-        get_neg_consts(A,HNegs,NegConsts),
-        append(PosConsts,NegConsts,Constrs).	
+split_affectation([],[]).      
+split_affectation([H],[Affect]) :-
+    split_string(H, "->", " ", [_,"",Affect]).         
+split_affectation([H|T],Vals) :-
+    split_affectation(T,Vals1),
+    split_string(H, "->", " ", [_,"",Affect]),
+    append([Affect],Vals1,Vals). 
+    
+       
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Transforming a list of strings representing a Z3 terms into a list 
+%  of Prolog terms
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+z3_to_term_list([],[]).
+z3_to_term_list([T|Z3Terms],Terms) :-
+        z3_to_term(T,Term),
+        z3_to_term_list(Z3Terms,Terms_),
+        append([Term],Terms_,Terms).
 
-%%  GERER PLUSIEURS ARGUMENTS !!! %%
-get_pos_consts(_,[],[]).
-get_pos_consts(A,[H|T],Constrs) :-
-        compound_name_arguments(H,_,[Args]),
-        C1 = [[A = (Args)]],
-        get_pos_consts(A,T,C2),
-        append(C1,C2,Constrs).
+z3_to_term(Z3Str,Term) :-
+        sub_string(Z3Str, 0, 1, _, "("),
+        sub_string(Z3Str, _, 1, 0, ")"),
+        sub_string(Z3Str, 1, _, 1, Str),!, 
+        split_string(Str, " ", "", [Name|Args]),
+        get_str_args(Args,StrArgs),
+        string_concat(Name,"(",Str1),
+        string_concat(Str1, StrArgs, Str2),
+        string_concat(Str2, ")", Str3),
+        term_string(Term, Str3).
+z3_to_term(Z3Str,Term) :- term_string(Term, Z3Str),!. 
 
-%%  Ajout de Fresh variables (Copy?) !!! %%
-get_neg_consts(_,[],[]).
-get_neg_consts(A,[H|T],Constrs) :-
-        compound_name_arguments(H,_,[Args]),
-	copy_term(Args,CArgs),
-	term_variables(CArgs,VCArgs),
-        forall_terms(VCArgs,A,CArgs,C1),
-        get_neg_consts(A,T,C2),
-        append([[C1]],C2,Constrs).
-
-forall_terms([],A,Args,Pred):- Pred = (A \= (Args)).   
-forall_terms([H|T],A,Args,Pred) :-
-        forall_terms(T,A,Args,Pred1),
-        Pred = forall(var(H),Pred1).    
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-grounding_subst(VarsToBeGrounded,Theta,B,Eta) :-
-	apply_term_subst_term(VarsToBeGrounded,Theta,VTBGTheta),
-	%
-	copy_term(VTBGTheta,VTBGThetac),
-	generate_ground_terms(VTBGThetac),
-	unifiable(VTBGTheta,VTBGThetac,Eta1),
-	%
-	compose_subst(Theta,Eta1,Sigma1),
-	range(Sigma1,B,NewVarsToBeGround),
-	%
-	copy_term(NewVarsToBeGround,NewVarsToBeGroundc),
-	generate_ground_terms(NewVarsToBeGroundc),
-	unifiable(NewVarsToBeGround,NewVarsToBeGroundc,Eta2),
-	%
-	compose_subst(Eta1,Eta2,Eta).
-
-%%%%
-
-domain([],[]).
-domain([V=_|Subst],[V|Dom]) :-
-	domain(Subst,Dom).
-
-domain(Subst,Atom,Dom) :-
-	domain(Subst,DomSubst),
-	term_variables(Atom,Vars),
-	strict_intersection(Vars,DomSubst,Dom).
-
-%%
-
-range(Subst,Ran) :-
-	range_aux(Subst,[],Ran).
-
-  range_aux([],R,R).
-  range_aux([_=T|Subst],R0,R) :-
-	term_variables(T,Vars),
-	strict_union(Vars,R0,R1),
-	range_aux(Subst,R1,R).
-
-
-range(Subst,Atom,Vars) :-
-	restrict_subst_atom_subst(Subst,Atom,SAtom),
-	range(SAtom,Vars).
-
-restrict_subst_atom_subst(S1,A,S2) :-
-	term_variables(A,Vars),
-	restrict_sas(S1,Vars,S2).
-
-  restrict_sas([],_Vars,[]).
-  restrict_sas([X=T|S1],Vars,[X=T|S2]) :-
-	strict_mem(X,Vars), !,
-	restrict_sas(S1,Vars,S2).
-  restrict_sas([_|S1],Vars,S2) :-
-	restrict_sas(S1,Vars,S2).
-
-
-
-%%
-
-apply_term_subst_term(Ground,_Subst,Ground) :-
-	ground(Ground), !.
-apply_term_subst_term(Var,Subst,Term) :-
-	var(Var), !, apply_var_subst_term(Var,Subst,Term).
-apply_term_subst_term(Term,Subst,Res) :-
-	functor(Term,F,N),functor(Res,F,N),
-	apply_term_subst_term(1,N,Term,Subst,Res).
-
-  apply_term_subst_term(P,N,_Term,_Subst,_Res) :- P > N, !.
-  apply_term_subst_term(I,N,Term,Subst,Res) :-
-	I =< N, J is I+1,
-	arg(I,Term,Ti), arg(I,Res,Ri),
-	apply_term_subst_term(Ti,Subst,Ri),
-	apply_term_subst_term(J,N,Term,Subst,Res).
-
-  apply_var_subst_term(Var,Subst,Term) :-
-	apply_subst_var_term(Subst,Var,Term).
-
-    apply_subst_var_term([Var1=Term1|_],Var,Term) :-
-	Var1 == Var, !, Term=Term1.
-    apply_subst_var_term([_|Subst],Var,Term) :-
-	apply_subst_var_term(Subst,Var,Term).
-    apply_subst_var_term([],Var,Var).
-
-%%
-
-compose_subst(S1,S2,S3) :-
-	apply_subst_subst(S1,S2,S12),
-	append(S12,S2,S4),
-	simplify_subst(S4,S3).
-
-  apply_subst_subst([],_S2,[]).
-  apply_subst_subst([X=T|S1],S2,[X=U|S3]) :-
-	apply_term_subst_term(T,S2,U),
-	apply_subst_subst(S1,S2,S3).
-
-  simplify_subst([],[]).
-  simplify_subst([X=T|Subst],[X=T|SimpSubst]) :-
-	X \== T, !, simplify_subst(Subst,SimpSubst).
-  simplify_subst([_|Subst],SimpSubst) :-
-	simplify_subst(Subst,SimpSubst).
-
-%% positive_atoms(+HPos,-B,-UVariablesInB)
-%% where HPos is a non-empty list
-positive_atoms(HPos,B,UVariablesInB) :-
-	HPos=[_|_],
-	copy_term(HPos,HPos0),
-	strict_shrink(HPos0,HPos1),
-	pa_1st_while(HPos1,HPos2),
-	pa_2nd_while(HPos2,B,[],Uvars),
-	term_variables(B,Bvars),
-	strict_intersection(Bvars,Uvars,UVariablesInB).
-
-
-pa_1st_while(HPos0,HPos) :-
-	append(As,[B1|HPos1],HPos0),
-	append(Bs,[B2|Cs],HPos1),
-	simple_disag_pair(B1,B2,P1,P2),
-	!,
-	P1=P2,
-	append([As,[B1|Bs],[B2|Cs]],HPos2),
-	strict_shrink(HPos2,HPos3),
-	pa_1st_while(HPos3,HPos).
-pa_1st_while(HPos,HPos).
-
-
-pa_2nd_while([B],B,Uvars,Uvars) :- !.
-pa_2nd_while([B1,B2|B2s],B,Uvars0,Uvars) :-
-	complex_disag_pair(B1,B2,_P1,_P2,X1-Tx1,X2-Tx2),
-	!,
-	X1=NewUVar,
-	X2=NewUVar,
-	Uvars1=[NewUVar|Uvars0],
-	strict_shrink([Tx1,Tx2|B2s],Cs),
-	pa_2nd_while(Cs,B,Uvars1,Uvars).
-
-%%%
-
-simple_disag_pair(T1,T2,_,_) :-
-	var(T1), occurs_check(T1,T2), !, fail.
-simple_disag_pair(T1,T2,_,_) :-
-	var(T2), occurs_check(T2,T1), !, fail.
-simple_disag_pair(T1,T2,P1,P2) :-
-	(var(T1) ; var(T2)), !,
-	P1=T1, P2=T2.
-simple_disag_pair(T1,T2,P1,P2) :-
-	functor(T1,F,N), functor(T2,F,N),
-	between(1,N,J),	arg(J,T1,T1j), arg(J,T2,T2j),
-	simple_disag_pair(T1j,T2j,P1,P2).
-
-  occurs_check(X,T) :- term_variables(T,VarsT), strict_mem(X,VarsT).
-
-
-complex_disag_pair(T1,T2,_,_,_,_) :-
-	var(T1), T1 == T2, !,
-	fail.
-complex_disag_pair(T1,T2,P1,P2,X1-Tx1,X2-Tx2) :-
-	(var(T1) ; var(T2)), !,
-	P1=T1, P2=T2, Tx1=X1, Tx2=X2.
-complex_disag_pair(T1,T2,P1,P2,X1-Tx1,X2-Tx2) :-
-	functor(T1,F,N), functor(T2,G,P), dif(F-N,G-P), !,
-	P1=T1, P2=T2,
-	Tx1=X1, Tx2=X2.
-complex_disag_pair(T1,T2,P1,P2,Y1-Tx1,Y2-Tx2) :-
-	functor(T1,F,N), functor(T2,F,N),
-	between(1,N,J),	arg(J,T1,T1j), arg(J,T2,T2j),
-	complex_disag_pair(T1j,T2j,P1,P2,Y1-Ty1,Y2-Ty2),
-	build_term(T1,J,Ty1,Tx1),
-	build_term(T2,J,Ty2,Tx2).
-
-  build_term(T,J,X,Tx) :-
-	functor(T,F,_N),
-	T=..[F|Args],
-	same_length(Args,Brgs),
-	Tx=..[F|Brgs],
-	build_children(J,Args,Brgs,X).
-
-    build_children(1,[_Arg|Args],[X|Args],X) :- !.
-    build_children(J,[A|As],[A|Bs],X) :-
-	J >= 2, K is J -1,
-	build_children(K,As,Bs,X).
-
-%%%
-
-
-strict_shrink([],[]).
-strict_shrink([A],[A]) :- !.
-strict_shrink([A1,A2|As],[A1|Bs]) :-
-	strict_not_mem(A1,[A2|As]), !,
-	strict_shrink([A2|As],Bs).
-strict_shrink([_A1,A2|As],Bs) :-
-	strict_shrink([A2|As],Bs).
-
-strict_intersection([],_,[]).
-strict_intersection([A|As],Bs,Cs) :-
-	strict_not_mem(A,Bs), !,
-	strict_intersection(As,Bs,Cs).
-strict_intersection([A|As],Bs,[A|Cs]) :-
-	strict_intersection(As,Bs,Cs).
-
-
-strict_union([],Bs,Bs).
-strict_union([X|Xs],Bs,Cs) :-
-	strict_mem(X,Bs), !,
-	strict_union(Xs,Bs,Cs).
-strict_union([X|Xs],Bs,[X|Cs]) :-
-	strict_union(Xs,Bs,Cs).
-
-%%%%
-
-generate_ground_terms([]).
-generate_ground_terms([X|XR]) :-
-  depthk(K),
-  term(K,X),
-  ground(X),
-  generate_ground_terms(XR). 
-  
- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
-strict_mem(X,[Y|_]) :- X == Y, !.
-strict_mem(X,[_|Ys]) :- strict_mem(X,Ys).
-strict_not_mem(X,Xs) :-
-	strict_not_mem_(Xs,X).
-
-  strict_not_mem_([],_X).
-  strict_not_mem_([Y|Zs],X) :-
-	Y \== X,
-	strict_not_mem_(Zs,X).
-
-strict_empty_intersection([],_).
-strict_empty_intersection([X|Xs],Vars) :-
-	strict_not_mem(X,Vars),
-	strict_empty_intersection(Xs,Vars).
-	
-strict_is_largely_incuded([],_).
-strict_is_largely_incuded([X|Xs],Ys) :-
-	strict_mem(X,Ys),
-	strict_is_largely_incuded(Xs,Ys).
+get_str_args([A],A).
+get_str_args([A|Args],StrArgs) :- 
+        get_str_args(Args,StrArgs_),
+        string_concat(A,",",Str),
+        string_concat(Str, StrArgs_, StrArgs).
+     
